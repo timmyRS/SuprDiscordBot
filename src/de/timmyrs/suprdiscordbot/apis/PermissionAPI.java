7c72dbf3cf60256405a86e6480e23ab6e736dca5
timmyRS <timmy14085@gmail.com>
2017-04-07 15:05:47 +0200

    Javadoc for some reason converts hex numbers to decimal numbers...

--- src/de/timmyrs/suprdiscordbot/apis/PermissionAPI.java
+++ src/de/timmyrs/suprdiscordbot/apis/PermissionAPI.java
@@ -13,111 +13,111 @@ import java.util.ArrayList;
 public class PermissionAPI
 {
 	/**
-	 * Allows creation of instant invites
+	 * Allows creation of instant invites (0x00000001)
 	 */
 	public final int CREATE_INSTANT_INVITE = 0x00000001;
 	/**
-	 * Allows kicking members
+	 * Allows kicking members (0x00000002)
 	 */
 	public final int KICK_MEMBERS = 0x00000002;
 	/**
-	 * Allows banning members
+	 * Allows banning members (0x00000004)
 	 */
 	public final int BAN_MEMBERS = 0x00000004;
 	/**
-	 * Allows all permissions and bypasses channel permission overwrites
+	 * Allows all permissions and bypasses channel permission overwrites (0x00000008)
 	 */
 	public final int ADMINISTRATOR = 0x00000008;
 	/**
-	 * Allows management and editing of channels
+	 * Allows management and editing of channels (0x00000010)
 	 */
 	public final int MANAGE_CHANNELS = 0x00000010;
 	/**
-	 * Allows management and editing of the guild
+	 * Allows management and editing of the guild (0x00000020)
 	 */
 	public final int MANAGE_GUILD = 0x00000020;
 	/**
-	 * Allows for the addition of reactions to messages
+	 * Allows for the addition of reactions to messages (0x00000040)
 	 */
 	public final int ADD_REACTIONS = 0x00000040;
 	/**
-	 * Allows reading messages in a channel. The channel will not appear for users without this permission
+	 * Allows reading messages in a channel. The channel will not appear for users without this permission (0x00000400)
 	 */
 	public final int READ_MESSAGES = 0x00000400;
 	/**
-	 * Allows for sending messages in a channel.
+	 * Allows for sending messages in a channel. (0x00000800)
 	 */
 	public final int SEND_MESSAGES = 0x00000800;
 	/**
-	 * Allows for sending of /tts messages
+	 * Allows for sending of /tts messages (0x00001000)
 	 */
 	public final int SEND_TTS_MESSAGES = 0x00001000;
 	/**
-	 * Allows for deletion of other users messages
+	 * Allows for deletion of other users messages (0x00002000)
 	 */
 	public final int MANAGE_MESSAGES = 0x00002000;
 	/**
-	 * Links sent by this user will be auto-embedded
+	 * Links sent by this user will be auto-embedded (0x00004000)
 	 */
 	public final int EMBED_LINKS = 0x00004000;
 	/**
-	 * Allows for uploading images and files
+	 * Allows for uploading images and files (0x00008000)
 	 */
 	public final int ATTACH_FILES = 0x00008000;
 	/**
-	 * Allows for reading of message history
+	 * Allows for reading of message history (0x00010000)
 	 */
 	public final int READ_MESSAGE_HISTORY = 0x00010000;
 	/**
-	 * Allows for using the @everyone tag to notify all users in a channel, and the @here tag to notify all online users in a channel
+	 * Allows for using the @everyone tag to notify all users in a channel, and the @here tag to notify all online users in a channel (0x00020000)
 	 */
 	public final int MENTION_EVERYONE = 0x00020000;
 	/**
-	 * Allows the usage of custom emojis from other servers
+	 * Allows the usage of custom emojis from other servers (0x00040000)
 	 */
 	public final int USE_EXTERNAL_EMOJIS = 0x00040000;
 	/**
-	 * Allows for joining of a voice channel
+	 * Allows for joining of a voice channel (0x00100000)
 	 */
 	public final int CONNECT = 0x00100000;
 	/**
-	 * Allows for speaking in a voice channel
+	 * Allows for speaking in a voice channel (0x00200000)
 	 */
 	public final int SPEAK = 0x00200000;
 	/**
-	 * Allows for muting members in a voice channel
+	 * Allows for muting members in a voice channel (0x00400000)
 	 */
 	public final int MUTE_MEMBERS = 0x00400000;
 	/**
-	 * Allows for deafening of members in a voice channel
+	 * Allows for deafening of members in a voice channel (0x00800000)
 	 */
 	public final int DEAFEN_MEMBERS = 0x00800000;
 	/**
-	 * Allows for moving of members between voice channels
+	 * Allows for moving of members between voice channels (0x01000000)
 	 */
 	public final int MOVE_MEMBERS = 0x01000000;
 	/**
-	 * Allows for using voice-activity-detection in a voice channel
+	 * Allows for using voice-activity-detection in a voice channel (0x02000000)
 	 */
 	public final int USE_VAD = 0x02000000;
 	/**
-	 * Allows for modification of own nickname
+	 * Allows for modification of own nickname (0x04000000)
 	 */
 	public final int CHANGE_NICKNAME = 0x04000000;
 	/**
-	 * Allows for modification of other users nicknames
+	 * Allows for modification of other users nicknames (0x08000000)
 	 */
 	public final int MANAGE_NICKNAMES = 0x08000000;
 	/**
-	 * Allows management and editing of roles
+	 * Allows management and editing of roles (0x10000000)
 	 */
 	public final int MANAGE_ROLES = 0x10000000;
 	/**
-	 * Allows management and editing of webhooks
+	 * Allows management and editing of webhooks (0x20000000)
 	 */
 	public final int MANAGE_WEBHOOKS = 0x20000000;
 	/**
-	 * Allows management and editing of emojis
+	 * Allows management and editing of emojis (0x40000000)
 	 */
 	public final int MANAGE_EMOJIS = 0x40000000;
 
