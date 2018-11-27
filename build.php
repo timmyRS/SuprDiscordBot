1319913f47a625f0364f597536cd11395b538ba1
Tim Speckhals <github@timmyrs.de>
2018-11-27 04:07:47 +0100

    Update build.php

--- build.php
+++ build.php
@@ -5,4 +5,11 @@ $zip=new ZipArchive();
 $zip->open("SuprDiscordBot.zip",ZipArchive::CREATE+ZipArchive::EXCL+ZipArchive::CHECKCONS)or die("Failed to create SuprDiscordBot.zip.\n");
 foreach(["LICENSE","readme.url","start.bat","start.sh","SuprDiscordBot.jar"]as$f)
 	$zip->addFile($f,$f);
+foreach(["scripts","scripts/disabled"]as$d)
+	foreach(scandir($d)as$f)
+	{
+		$p="$d/$f";
+		if(is_file($p))
+			$zip->addFile($p,$p);
+	}
 $zip->close();
