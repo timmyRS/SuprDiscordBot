ebadc11044e3ec8b912dd1981b4009cba283e053
Tim Speckhals <github@timmyrs.de>
2018-11-27 03:54:39 +0100

    Move dist to root

--- build.php
+++ build.php
@@ -3,8 +3,6 @@ if(file_exists("SuprDiscordBot.zip"))
 	unlink("SuprDiscordBot.zip");
 $zip=new ZipArchive();
 $zip->open("SuprDiscordBot.zip",ZipArchive::CREATE+ZipArchive::EXCL+ZipArchive::CHECKCONS)or die("Failed to create SuprDiscordBot.zip.\n");
-foreach(["readme.url","start.bat","start.sh","SuprDiscordBot.jar"]as$f)
+foreach(["LICENSE","readme.url","start.bat","start.sh","SuprDiscordBot.jar"]as$f)
 	$zip->addFile($f,$f);
-foreach(["LICENSE"]as$f)
-	$zip->addFile("../$f",$f);
 $zip->close();
