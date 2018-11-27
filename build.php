dc6dd80553d5d960225c1ad4e6ab08d0049049ee
Tim Speckhals <github@timmyrs.de>
2018-11-27 06:21:16 +0100

    Fully mavenize

--- build.php
+++ build.php
@@ -2,8 +2,14 @@
 if(file_exists("SuprDiscordBot.zip"))
 	unlink("SuprDiscordBot.zip");
 $zip=new ZipArchive();
-$zip->open("SuprDiscordBot.zip",ZipArchive::CREATE+ZipArchive::EXCL+ZipArchive::CHECKCONS)or die("Failed to create SuprDiscordBot.zip.\n");
-foreach(["LICENSE","readme.url","start.bat","start.sh","SuprDiscordBot.jar"]as$f)
+$zip->open("SuprDiscordBot.zip",ZipArchive::CREATE+ZipArchive::EXCL+ZipArchive::CHECKCONS)||die("Failed to create zipfile\n");
+foreach(scandir("target")as$f)
+{
+	$p="target/$f";
+	if(is_file($p))
+		$zip->addFile($p,"SuprDiscordBot.jar");
+}
+foreach(["LICENSE","readme.url","start.bat","start.sh"]as$f)
 	$zip->addFile($f,$f);
 foreach(["scripts","scripts/disabled"]as$d)
 	foreach(scandir($d)as$f)
